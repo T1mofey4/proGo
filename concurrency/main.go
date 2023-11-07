58dc38e46184fe763145c7af2c8997e10771247a
T1mofey4 <timofeyvalerievich@gmail.com>
2023-11-07 17:48:37 +0300

    concurrency

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	fmt.Println("main function started")
 	CalcStoreTotal(Products)
-	time.Sleep(time.Second * 5)
+	// time.Sleep(time.Second * 5)
 	fmt.Println("main function complete")
 
+	dispatchChannel := make(chan DispatchNotification, 100)
+	go DispatchOrders(dispatchChannel)
+	for {
+		if details, open := <-dispatchChannel; open {
+			fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+		} else {
+			fmt.Println("Channel has been closed")
+			break
+		}
+	}
 }
